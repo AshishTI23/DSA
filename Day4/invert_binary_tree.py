fa9e6f41db30f3289dcf2fa19299897cb98e8fb8
Ashish <ashish.nitdgp16@gmail.com>
2023-08-20 01:22:04 +0530

    invert binary tree

---
+++ Day4/invert_binary_tree.py
@@ -0,0 +1,22 @@
+# August-20-2023
+
+# Given the root of a binary tree, invert the tree, and return its root
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+
+    def invert_binary_tree_with_recursion(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invert_binary_tree(root.left)
+        self.invert_binary_tree(root.right)
+
+        return root
