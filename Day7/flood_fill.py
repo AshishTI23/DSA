0a5997f8ae4ea35fa26a51fe33c0b74baedba62f
Ashish <ashish.nitdgp16@gmail.com>
2023-08-22 22:33:19 +0530

    Solving date corrected

--- Day7/flood_fill.py
+++ Day7/flood_fill.py
@@ -1,10 +1,11 @@
-# August-20-2023
+# August-22-2023
 
 # An image is represented by an m x n integer grid image where image[i][j]
 # represents the pixel value of the image.
 # You are also given three integers sr , sc and color . You should perform a
 # flood fill on the image starting from the pixel image[sr][sc] .
 # Return the modified image after performing the flood fill.
+
 # Input: image = {{1,1,1},{1,1,0},{1,0,1}},
 # sr = 1, sc = 1, newColor = 2.
 # Output: {{2,2,2},{2,2,0},{2,0,1}}
