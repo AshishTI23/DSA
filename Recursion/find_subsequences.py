30c0bf24bfd05789480cacd1daa62efe0db35f01
Ashish <ashish.nitdgp16@gmail.com>
2023-08-26 20:49:09 +0530

    Find all subsequences solved

---
+++ Recursion/find_subsequences.py
@@ -0,0 +1,22 @@
+# August-24-2023
+
+# Print All Subsequnces
+from typing import List
+
+
+class Solution:
+    def __init__(self, sequence: List[int]):
+        self.sequence = sequence
+
+    def print_subsequnces(self, index: int = 0, subsequence_array: List[int] = []):
+        global subsequence_list
+        if index >= len(self.sequence):
+            print(subsequence_array)
+            return
+        subsequence_array.append(self.sequence[index])
+        self.print_subsequnces(index + 1, subsequence_array)
+        subsequence_array.pop()
+        self.print_subsequnces(index + 1, subsequence_array)
+
+
+Solution([3, 1, 2]).print_subsequnces()
